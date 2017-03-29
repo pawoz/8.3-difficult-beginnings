998fd86330eb7b73d88f11e55f6d8fcbf5444203
pawoz <pawel.wozny@op.pl>
2017-03-29 21:36:14 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// comment#1
+/*
+comment#2
+*/
+var name = prompt('Wpisz swoje imię');
+// alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+/*
+var zmienna = 'test';
+console.log(zmienna);
+*/
